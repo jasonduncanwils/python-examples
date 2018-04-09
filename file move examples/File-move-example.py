f49ef0146ae403567b97fb5db69fe046d815ab26
jasonduncanwilson <jason@duncanwilsongroup.com>
2018-04-09 15:44:41 -0400

    Finished comments on existing scripts

--- file move examples/File-move-example.py
+++ file move examples/File-move-example.py
@@ -1,9 +1,12 @@
 import shutil, sys, time, os
+
+# general variables
 src = 'C:\Source'
 dst = 'C:\Destination'
-
 now = time.time()
 print (now)
+
+# iterate through files to move them
 for path, dirs, files in os.walk(src):
 	for f in files:
 		f = os.path.join(path, f)
